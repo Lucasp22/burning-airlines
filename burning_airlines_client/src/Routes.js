308e5f2731252699e09520e74e29dc8d9a27520f
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-07-31 19:59:10 +1000

    fixed routing will work now

--- burning_airlines_client/src/Routes.js
+++ burning_airlines_client/src/Routes.js
@@ -3,14 +3,14 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 // import Home from './components/Home';
 // import FAQ from './components/FAQ';
 // import Character from './components/Character'
+import FlightInfo from './components/FlightInfo'
 
 
 const Routes = (
   <Router>
     <div>
-      // <Route exact path = "/" component = { Home } />
-      // <Route exact path = "/faq" component = { FAQ } />
-      // <Route path = "/characters/:name" component = { Character } />
+
+      <Route exact path = "/flightinfo" component = {FlightInfo} />
     </div>
   </Router>
 )
