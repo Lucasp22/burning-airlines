99c3d629702f6b44e503313263cde9f7d7dd6409
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-07-31 19:33:15 +1000

    added children of FlightInfo Component

--- burning_airlines_client/src/components/FlightInfo.js
+++ burning_airlines_client/src/components/FlightInfo.js
@@ -10,4 +10,23 @@ class FlightInfo extends Component {
   }
 }
 
+class Seats extends Component {
+  render(){
+    return(
+      <div>
+      <h1> Seats coming soon </h1>
+      </div>
+    )
+  }
+}
+
+class ConfirmSeat extends Component {
+  render(){
+    return(
+      <div>
+      <h1> Confirm Seats coming soon </h1>
+      </div>
+    )
+  }
+}
 export default FlightInfo;
