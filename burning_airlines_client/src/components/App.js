9659f5dd7d8adee857c91d4676020109a9aa2780
ellijayne <ellijayne.m.willard@gmail.com>
2018-07-31 19:02:11 +1000

    created components folder in src folder, moved app.js to components, created SearchFlight.js for EJ part of project

---
+++ burning_airlines_client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Flight app coming soon</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ burning_airlines_client/src/components/SearchFlight.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+//need to install npm package once i have file so that i can use the below:
+import { Link } from 'react-router-dom';
+
+class SearchFlights extends Component {
+  render() {
+    return(
+      <div>
+        <SearchForm />
+        <FlightResults />
+      </div>
+    )
+  }
+}
+
+class SearchForm extends Component {
+  render() {
+    return(
+      <form>
+        <input type="text" placeholder="Origin" required autofocus>
+        <input type="text" placeholder="Destination" required>
+        <input type="submit" value="Go">
+      </form>
+    )
+  }
+}
+
+class FlightResults extends Component {
+  render() {
+      const flights = ['QF1', 'VG2', 'JS9'];
+    return(
+      //fix this when functional - want link to be dynamic and link to particular flight related to the link using
+
+      //going to take the flight info and loop through to push links for each onto page....
+
+      //this is the flight info in array:
+      <ul>
+        {flights.map(function(flight) {
+          //below is not quote complete, later with more information and values for flights i will need to pull out from the array of objects the flight id or the flight number or something.
+          return <li><Link to={`/flight/${flight}`}></li>;
+        })};
+      </ul>
+
+
+
+      <Link to="/flight"> //this will be the only link you have to hard code cos you will wrap the loop around this, so it will just print however many you have
+    )
+  }
+}
