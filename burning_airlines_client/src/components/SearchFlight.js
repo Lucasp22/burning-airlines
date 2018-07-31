d87d6c83ce9f9e6ade4b37341e589e9b95ee27ee
ellijayne <ellijayne.m.willard@gmail.com>
2018-07-31 22:18:23 +1000

    fixing look of SearchFlight render

--- burning_airlines_client/src/components/SearchFlight.js
+++ burning_airlines_client/src/components/SearchFlight.js
@@ -29,7 +29,10 @@ class SearchForm extends Component {
 class FlightResults extends Component {
   render() {
       const flights = ['QF1', 'VG2', 'JS9'];
-      //    {date: "02/08/2018", Flight: 'QF1', FromTo: "SYD > MEL", plane: 747},{date: "03/08/2018", Flight: 'JS002', FromTo: "SYD > JFK", plane: 747},{date: "04/08/2018", Flight: 'JAL178', FromTo: "SYD > NRT", plane: 747}
+      // [
+      // {date: "02/08/2018", Flight: 'QF1', FromTo: "SYD > MEL", plane: 747},{date: "03/08/2018", Flight: 'JS002', FromTo: "SYD > JFK", plane: 747},{date: "04/08/2018", Flight: 'JAL178', FromTo: "SYD > NRT", plane: 747}
+      // ]
+
 
     return(
       //fix this when functional - want link to be dynamic and link to particular flight related to the link using
@@ -43,7 +46,7 @@ class FlightResults extends Component {
         <ul>
           {flights.map(function(flight) {
             //below is not quote complete, later with more information and values for flights i will need to pull out from the array of objects the flight id or the flight number or something.
-            return <li><Link to={`/flight/${flight}`}>flight:{flight}</Link></li>;
+            return <li>Flight: <Link to={`/flight`}>{flight}</Link></li>;
             // return <li>flight:{flight}</li>
           })}
         </ul>
