8d8edd42284d0b4db1ee708bf2b69cfcb9965be8
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-07-31 19:51:44 +1000

    added routing and copied basic stuff from joels routing intro

---
+++ burning_airlines_client/src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import FAQ from './components/FAQ';
+// import Character from './components/Character'
+
+
+const Routes = (
+  <Router>
+    <div>
+      // <Route exact path = "/" component = { Home } />
+      // <Route exact path = "/faq" component = { FAQ } />
+      // <Route path = "/characters/:name" component = { Character } />
+    </div>
+  </Router>
+)
+
+export default Routes;
