fa57a9f330dd47766928854698ac1c2ce9a7664b
ellijayne <ellijayne.m.willard@gmail.com>
2018-08-01 12:07:25 +1000

    added drop down to new flight form server side

--- burning_airlines_server/db/seeds.rb
+++ burning_airlines_server/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Plane.destroy_all
+plane1 = :name => "747", :row => 58, :column => "4", :seats => 232
+
+plane2 = :name => "787", :row => 24, :column => "4", :seats => 98
+
+
+Flight.destroy_all
+flight1 = Flight.create :flight_name => 'QF01', :origin => "SYD", :destination: => "MEL", :date => "2018-08-02", :plane_id => plane1.id
+
+flight2 = Flight.create :flight_name => 'BA989', :origin => "LDN", :destination: => "SYD", :date => "2018-08-03", :plane_id => plane2.id
+
+flight3 = Flight.create :flight_name => 'QF2', :origin => "SYD", :destination: => "MEL", :date => "2018-08-04", :plane_id => plane1.id
+
+flight4 = Flight.create :flight_name => 'BA977', :origin => "LDN", :destination: => "SYD", :date => "2018-08-06", :plane_id => plane2.id
