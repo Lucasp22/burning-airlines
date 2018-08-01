2b7728b472dbea94dd066f4ee9887a103982324f
Lucas Padua <lucas.p2222@gmail.ciom>
2018-08-01 10:15:23 +1000

    Home.js

--- burning_airlines_client/src/components/Home.js
+++ burning_airlines_client/src/components/Home.js
@@ -1,76 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
-class LoginPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: '',
-      password: '',
-      error: '',
-    };
-
-    this.handlePassChange = this.handlePassChange.bind(this);
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.dismissError = this.dismissError.bind(this);
-  }
-
-  dismissError() {
-    this.setState({ error: '' });
-  }
-
-  handleSubmit(evt) {
-    evt.preventDefault();
-
-    if (!this.state.username) {
-      return this.setState({ error: 'Username is required' });
-    }
-
-    if (!this.state.password) {
-      return this.setState({ error: 'Password is required' });
-    }
-
-    return this.setState({ error: '' });
-  }
-
-  handleUserChange(evt) {
-    this.setState({
-      username: evt.target.value,
-    });
-  };
-
-  handlePassChange(evt) {
-    this.setState({
-      password: evt.target.value,
-    });
-  }
-
+class Home extends Component {
   render() {
-    // NOTE: I use data-attributes for easier E2E testing
-    // but you don't need to target those (any css-selector will work)
-
-    return (
-      <div className="Login">
-        <form onSubmit={this.handleSubmit}>
-          {
-            this.state.error &&
-            <h3 data-test="error" onClick={this.dismissError}>
-              <button onClick={this.dismissError}>✖</button>
-              {this.state.error}
-            </h3>
-          }
-          <label>User Name</label>
-          <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
-
-          <label>Password</label>
-          <input type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
-
-          <input type="submit" value="Log In" data-test="submit" />
-        </form>
+    return(
+      <div>
+      <LoginForm />
       </div>
     );
   }
+};
+class LoginForm extends Component {
+  render() {
+    return(
+      <form>
+        <h2>Login</h2>
+      <input name="user_name"type="text" placeholder="User Name" required autofocus />
+      <input type="text" placeholder="Password" required />
+      <input name="submit" type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
-
-export default LoginPage;
+export default Home;
