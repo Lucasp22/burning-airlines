b1b48eef64d9bd22b7a7f771e85210a9f22c365a
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-08-02 11:44:43 +1000

    link of flights to plane

--- burning_airlines_client/src/components/SearchFlight.js
+++ burning_airlines_client/src/components/SearchFlight.js
@@ -64,7 +64,7 @@ getFlights(origin, destination){
         {/* this is like gallery */}
         <FlightResults flights={this.state.flights} />
         <ul>
-          {this.state.resultFlights.map( (result) => <li>{result.flight_name}</li>)}
+          {this.state.resultFlights.map( (result) => <Link to ={`/flightinfo/${result.plane_id}`}><li>{result.flight_name}</li></Link>)}
         </ul>
 
       </div>
