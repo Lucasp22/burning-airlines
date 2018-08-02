d6b86dca420ec16dc999ac9262df8a2c12e38e29
ellijayne <ellijayne.m.willard@gmail.com>
2018-08-02 10:55:14 +1000

    have search results pushing to page based on json request

--- burning_airlines_client/src/components/SearchFlight.js
+++ burning_airlines_client/src/components/SearchFlight.js
@@ -9,34 +9,51 @@ class SearchFlights extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       // flights: [{flight: 'QF100'}, {flight: 'VG2'}, {flight: 'JS9'}]
-       flights: []
-       // origin: '',
-       // destination: ''
+       flights: [],
+       resultFlights: []
   };
-   this.getflights = this.getFlights.bind(this);
+  this.getFlights = this.getFlights.bind(this);
 
 }
 
-  //this is syntax for fat arrow function.
-  //pulled in all my json data
-  getFlights = (origin, destination) => {
-    // console.log("something");
-    axios.get(SERVER_URL).then(function(results){
-      // const userSearch = results.map(function(data) {
-      // return<li>Flights: <Link to={`/flight`}>{results.data.flight_name}</Link></li>;
-      // })
-      console.log("results",results);
+//this is syntax for fat arrow function.
+//pulled in all my json data
+getFlights(origin, destination){
+  // console.log(origin, destination);
+  // console.log("something");
+  axios.get(SERVER_URL).then((results) => {
+    const searchResult = results.data;
+    let resultFlights = []
+    for (var i = 0; i < searchResult.length; i++) {
+      // console.log(searchResult[i]);
+      console.log(searchResult[i].origin, origin, searchResult[i].destination , destination);
+      if ((searchResult[i].origin === origin) && (searchResult[i].destination === destination)) {
+      resultFlights.push(searchResult[i]);
+      }
+    }
+    console.warn(resultFlights)
+    this.setState({resultFlights: resultFlights})
+//list o n screen
+//making the getFlights array needs to be passed to search results as a prop
+    // console.log(searchResult);
 
-      // this.setState({flights: results.data});
-      //want to be able to take away the hard coding above,
-      //let user input their origin and Destination
-      //update state with results setState flights ======== whatever.......
-
-      //inside here need this.setState for search flights, do something with the results and map it or get it only screen
     })
+    // return  <li>Flights: <Link to={`/flightinfo/:id`}>{data}</Link></li>;
 
+    // this.setState({ results.data })
+    // const userSearch = results.map(function(data) {
+    // return<li>Flights: <Link to={`/flight`}>{results.data.flight_name}</Link></li>;
+    // })
+    // console.log("results",results);
+
+    // this.setState({flights: results.data});
+    //want to be able to take away the hard coding above,
+    //let user input their origin and Destination
+    //update state with results setState flights ======== whatever.......
+
+    //inside here need this.setState for search flights, do something with the results and map it or get it only screen
   }
+
 
 
   render() {
@@ -46,34 +63,68 @@ class SearchFlights extends Component {
         <SearchForm onSubmit={this.getFlights}/>
         {/* this is like gallery */}
         <FlightResults flights={this.state.flights} />
+        <ul>
+          {this.state.resultFlights.map( (result) => <li>{result.flight_name}</li>)}
+        </ul>
+
       </div>
     )
   }
 }
 
+
 class SearchForm extends Component {
   constructor() {
     super();
-    this.state = {query: ''}
+    this.state = {origin: '', destination: ''}
     this._handleSubmit = this._handleSubmit.bind(this);
-    this._handleInput = this._handleInput.bind(this);
+    this._handleOriginChange = this._handleOriginChange.bind(this);
+    this._handleDestinationChange = this._handleDestinationChange.bind(this);
+
   }
 
   _handleSubmit(event) {
     event.preventDefault();
-    this.props.onSubmit(this.state.query);
+    this.props.onSubmit(this.state.origin, this.state.destination);
   }
 
-  _handleInput(event) {
-    console.log(event.target.value);
-     this.setState({query: event.target.value})
-  }
+    _handleOriginChange(event) {
+      // console.log(event.target.value);
+      this.setState( { origin: event.target.value } );
+      console.log(this.state);
+    }
+
+    _handleDestinationChange(event) {
+      // console.log(event.target.value);
+    this.setState( { destination: event.target.value } );
+    console.log(this.state);
+    }
+
 
   render() {
     return(
       <form onSubmit={this._handleSubmit}>
-        <input type="text" placeholder="Origin" required autoFocus onInput={this._handleInput} />
-        <input type="text" placeholder="Destination" required onInput={this._handleInput} />
+
+        <input type="text" placeholder="Origin" required autoFocus onInput={this._handleOriginChange} />
+        <input type="text" placeholder="Destination" required onInput={this._handleDestinationChange} />
+
+
+        {/*TODO <p>Origin</p>
+          <select value={this.state.value} onChange={this._handleOriginChange}>
+          <option value=""></option>
+          <option value="SYD">SYD</option>
+          <option value="MEL">MEL</option>
+          <option value="NRT">NRT</option>
+          </select>
+        <p>Destination</p>
+          <select value={this.state.value} onChange={this._handleDestinationChange}>
+          <option value=""></option>
+          <option value="SYD">SYD</option>
+          <option value="MEL">MEL</option>
+          <option value="NRT">NRT</option>
+          </select> */}
+
+
         <input type="submit" value="Go" />
       </form>
     )
@@ -82,7 +133,7 @@ class SearchForm extends Component {
 
 class FlightResults extends Component {
   render() {
-      // const flights = ['QF1', 'VG2', 'JS9'];
+
     return(
       //fix this when functional - want link to be dynamic and link to particular flight related to the link using
 
